c765ee19169d6698f3caadbe649832e4b122f0b6
Ylber Prapashtica <ylberprapashtica@gmail.com>
2024-12-04 18:41:10 +0100

    Veprim Aritmetikor i variablave numra

--- main.js
+++ main.js
@@ -3,3 +3,19 @@ let titulli_i_faqes;
 titulli_i_faqes = "Koha Ditore";
 
 document.getElementById('title').innerHTML = titulli_i_faqes;
+
+debugger;
+
+let njerez_ne_lift;
+njerez_ne_lift = 14;
+
+let pesha_per_person;
+pesha_per_person = 90.3;
+
+pesha_per_person = 10.2;
+
+let pesha_ne_lift;
+pesha_ne_lift = njerez_ne_lift * pesha_per_person;
+
+document.getElementById('njerz-ne-lift').innerHTML = njerez_ne_lift;
+document.getElementById('pesha-ne-lift').innerHTML = pesha_ne_lift;
