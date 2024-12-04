721aa74c3659efcd1cf32ac67a81e3f89d8a104a
Ylber Prapashtica <ylberprapashtica@gmail.com>
2024-12-04 19:18:45 +0100

    Funksionet

--- main.js
+++ main.js
@@ -10,8 +10,6 @@ titulli_i_faqes = fjala1 + " " + fjala2;
 
 document.getElementById('title').innerHTML = titulli_i_faqes;
 
-debugger;
-
 let njerez_ne_lift;
 njerez_ne_lift = 14;
 
@@ -25,3 +23,28 @@ pesha_ne_lift = njerez_ne_lift * pesha_per_person;
 
 document.getElementById('njerz-ne-lift').innerHTML = njerez_ne_lift;
 document.getElementById('pesha-ne-lift').innerHTML = pesha_ne_lift;
+
+debugger;
+
+function kalkuloPermbatjenEStringutTeLajmeveTeDites(perseritjet) {
+    let permbatja_e_elementit_lajmet_e_dites;
+    permbatja_e_elementit_lajmet_e_dites = "";
+
+    for (let index = 0; index < perseritjet; index++) {
+        permbatja_e_elementit_lajmet_e_dites = permbatja_e_elementit_lajmet_e_dites + "<article></article>";
+    }
+
+    return permbatja_e_elementit_lajmet_e_dites;
+}
+
+let numri_i_artikujve;
+numri_i_artikujve = 10;
+
+let te_gjitha_lajmet;
+te_gjitha_lajmet = kalkuloPermbatjenEStringutTeLajmeveTeDites(numri_i_artikujve);
+document.getElementById('lajmet-e-dites').innerHTML = te_gjitha_lajmet;
+
+let gjysa_e_lajmeve;
+gjysa_e_lajmeve = kalkuloPermbatjenEStringutTeLajmeveTeDites(numri_i_artikujve / 2);
+document.getElementById('lajmet-e-dites-2').innerHTML = gjysa_e_lajmeve;
+
