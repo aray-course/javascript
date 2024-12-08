faead28cb505ffc0923a9a3459c94e121952710c
Ylber Prapashtica <ylberprapashtica@gmail.com>
2024-12-08 19:57:27 +0100

    Add Run Calculator

--- main.js
+++ main.js
@@ -1,30 +1,10 @@
-let variabla1;
-variabla1 = 0; 
+// Koha e vrapimit ne kilometer per ore
+let shpejtsia_e_vrapimit = 18;
+// Koha e vrapimit ne ore
+let koha_e_vrapimit = 5;
 
-let variabla4 = 1000;
+let distanca = shpejtsia_e_vrapimit * koha_e_vrapimit;
 
-let variabla2;
-variabla2 = "ylber";
-
-let variabla3;
-variabla3 = null;
-
-console.log(variabla1);
-console.log(variabla2);
-console.log(variabla3);
-console.log(variabla4);
-
-variabla1 = 10;
-console.log(variabla1);
-
-variabla1 = 100;
-console.log(variabla1);
-
-variabla1 = 100 + 100;
-console.log(variabla1);
-
-variabla1 = 100 + 100 + 100;
-console.log(variabla1);
-
-variabla1 = 100 + variabla4;
-console.log(variabla1);
+document.getElementById("shpejtesia-e-vrapimit").innerHTML = shpejtsia_e_vrapimit;
+document.getElementById("koha-e-vrapimit").innerHTML = koha_e_vrapimit;
+document.getElementById("distanca-e-vrapimit").innerHTML = distanca;
