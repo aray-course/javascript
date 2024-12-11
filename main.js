a0500d224a63a77ae98c96c9716de5a8dc06af62
Ylber Prapashtica <ylberprapashtica@gmail.com>
2024-12-11 20:52:44 +0100

    if else

--- main.js
+++ main.js
@@ -1,29 +1,23 @@
-debugger;
-let distanca;
-function kalkuloDistanceDheKalorite(shpejtsia_e_vrapimit, koha_e_vrapimit, kalorite_e_djegura_per_km) {
-    distanca = shpejtsia_e_vrapimit * koha_e_vrapimit;
+const legalAdultAge = 18;
 
-    console.log("Shpejtesia e vrapimit eshte " + shpejtsia_e_vrapimit +" km/h");
-    console.log("Koha e vrapimit eshte " + koha_e_vrapimit + " h");
-    console.log("Distanca e vrapuar eshte " + distanca + " km");
-    console.log("Kalorite e djegura jane " + distanca * kalorite_e_djegura_per_km + " c");
-    console.log("---")
+//== is it equal or is not different
+// < smaller than
+//<= smaller than or equal
+// > greater than
+//>= greater than or equal
+//!= not equal or is different
+//&& and
+//|| ose
+function canIDrink(myAge){
+    debugger;
+    if(myAge < legalAdultAge){
+        console.log("I am NOT old enough to drink");
+    }else if(myAge >= 120){
+        console.log("I am dead");
+    }else if(myAge >= 50){
+        console.log("I am too old to drink");
+    }else{
+        console.log("I am old enough to drink");
+    }
 }
 
-function kalkuloDistanceDheNxehtisne(shpejtsia_e_vrapimit, koha_e_vrapimit, nxetesia_e_krijuar_per_km) {
-    distanca = shpejtsia_e_vrapimit * koha_e_vrapimit;
-
-    console.log("Shpejtesia e vrapimit eshte " + shpejtsia_e_vrapimit +" km/h");
-    console.log("Koha e vrapimit eshte " + koha_e_vrapimit + " h");
-    console.log("Distanca e vrapuar eshte " + distanca + " km");
-    console.log("Nxehtisine trupore e krijuar eshte " + distanca * nxetesia_e_krijuar_per_km + " C");
-    console.log("---")
-}
-
-function tregoDistancen() {
-    console.log(distanca);
-}
-
-kalkuloDistanceDheKalorite(10, 3, 10);
-kalkuloDistanceDheNxehtisne(10, 3, 10);
-console.log(distanca);
