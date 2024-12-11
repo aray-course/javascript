995437f2a97c670425c177b9ea65a4daf3a0ca90
Ylber Prapashtica <ylberprapashtica@gmail.com>
2024-12-11 19:36:38 +0100

    New commit

--- main.js
+++ main.js
@@ -1,23 +1,7 @@
 debugger;
 
 function kalkuloDistanceDheKalorite(shpejtsia_e_vrapimit, koha_e_vrapimit, kalorite_e_djegura_per_km) {
-    let distanca = shpejtsia_e_vrapimit * koha_e_vrapimit;
-
-    console.log("Shpejtesia e vrapimit eshte " + shpejtsia_e_vrapimit +" km/h");
-    console.log("Koha e vrapimit eshte " + koha_e_vrapimit + " h");
-    console.log("Distanca e vrapuar eshte " + distanca + " km");
-    console.log("Kalorite e djegura jane " + distanca * kalorite_e_djegura_per_km + " c");
-    console.log("---")
-    let text = `<div class="test"><p>Shpejtesia e vrapimit eshte ${shpejtsia_e_vrapimit} km/h</p>
-    <p>Koha e vrapimit eshte ${koha_e_vrapimit} h</p>
-    <p>Distanca e vrapuar eshte ${distanca} km</p>
-    <p>Kalorite e djegura jane ${distanca * kalorite_e_djegura_per_km} c</p></div>`;
-
-    let content = document.getElementById('content').innerHTML;
-
-    document.getElementById('content').innerHTML = content + text;
-
-
+   
 }
 
 kalkuloDistanceDheKalorite(10, 3, 10);
