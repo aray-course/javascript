2f4b01dbf89d12a06d6c5975a1a070844cc22c53
Ylber Prapashtica <ylberprapashtica@gmail.com>
2024-12-11 20:34:59 +0100

    Scope

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
-var firstname = "Redon";
-var lastname = "Tahiri";
+debugger;
+let distanca;
+function kalkuloDistanceDheKalorite(shpejtsia_e_vrapimit, koha_e_vrapimit, kalorite_e_djegura_per_km) {
+    distanca = shpejtsia_e_vrapimit * koha_e_vrapimit;
 
-console.log("1:", firstname, lastname);
-
-function getFullName(firstname, lastname, vjet) {
-    firstname = "Bardh";
-    return firstname + ' ' + lastname + ' i ka ' + vjet + ' vjet';
+    console.log("Shpejtesia e vrapimit eshte " + shpejtsia_e_vrapimit +" km/h");
+    console.log("Koha e vrapimit eshte " + koha_e_vrapimit + " h");
+    console.log("Distanca e vrapuar eshte " + distanca + " km");
+    console.log("Kalorite e djegura jane " + distanca * kalorite_e_djegura_per_km + " c");
+    console.log("---")
 }
 
-function showAlert() {
-    debugger;
-    let test = getFullName(firstname, lastname, 21);
-    alert(test)
+function kalkuloDistanceDheNxehtisne(shpejtsia_e_vrapimit, koha_e_vrapimit, nxetesia_e_krijuar_per_km) {
+    distanca = shpejtsia_e_vrapimit * koha_e_vrapimit;
+
+    console.log("Shpejtesia e vrapimit eshte " + shpejtsia_e_vrapimit +" km/h");
+    console.log("Koha e vrapimit eshte " + koha_e_vrapimit + " h");
+    console.log("Distanca e vrapuar eshte " + distanca + " km");
+    console.log("Nxehtisine trupore e krijuar eshte " + distanca * nxetesia_e_krijuar_per_km + " C");
+    console.log("---")
 }
 
-console.log('2:', "Fullname is", getFullName());
+function tregoDistancen() {
+    console.log(distanca);
+}
+
+kalkuloDistanceDheKalorite(10, 3, 10);
+kalkuloDistanceDheNxehtisne(10, 3, 10);
+console.log(distanca);
